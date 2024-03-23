e19f710921f033bb1c4834d9cfb816eeeaccb931
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-03-23 08:18:24 +0530

    wer

--- app/page.js
+++ app/page.js
@@ -27,6 +27,33 @@ export default function Home() {
 
 </div>
 
+<p className="text-white text-center text-2xl font-bold font-mono mt-[-27px]">Join Our Community</p>
+<div className="flex px-4 gap-6 justify-center items-center mt-7 ">
+<div>
+<img className=" w-[300px]" src="telegramlogo.png"/>
+</div>
+<div className="flex flex-col items-center">
+  <h1 className="text-white text-center text-xl font-bold">Telegram Channel</h1>
+  <p className="text-white text-sm text-center mt-2">Your essential channel
+ for expert insights, trading tips,
+ market analysis, and discussions 
+on the latest trends in cryptocurrency.</p>
+<a href="#"><button className="bg-yellow-400 px-6 py-1 rounded-md font-bold mt-3">Join Now</button></a>
+</div>
+</div>
+<div className="flex px-4 gap-6 justify-center items-center mt-7 ">
+<div>
+<img className=" w-[300px]" src="telegramlogo.png"/>
+</div>
+<div className="flex flex-col items-center">
+  <h1 className="text-white text-center text-xl font-bold">Telegram Channel</h1>
+  <p className="text-white text-sm text-center mt-2">Your essential channel
+ for expert insights, trading tips,
+ market analysis, and discussions 
+on the latest trends in cryptocurrency.</p>
+<a href="#"><button className="bg-yellow-400 px-6 py-1 rounded-md font-bold mt-3">Join Now</button></a>
+</div>
+</div>
 
 </div>   
 
